8072c59950e6c6b5cd7daca50c3db9fbdd097a2b
Atul <atulkhadoliya10@gmail.com>
2025-05-31 00:06:23 +0530

    Add: Health check endpoint for Render deployment

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 const PORT =  process.env.PORT || 3000;
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Code editor backend is alive and well!');
+});
 
 const server = app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
